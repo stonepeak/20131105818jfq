114d03956e37ab22f3101904a042355e0e47d68f
stonepeak <dou@Mac18.local>
2016-04-18 21:23:40 +0800

    🏀篮球计分器4.18
    最初的界面设计

---
+++ 20131105818jfq/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  20131105818jfq
+//
+//  Created by dou on 16/4/18.
+//  Copyright © 2016年 stonepeak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func vs(sender: UIButton) {
+        
+    }
+
+    @IBOutlet weak var k1: UITextField!
+    @IBOutlet weak var k2: UITextField!
+}
+
